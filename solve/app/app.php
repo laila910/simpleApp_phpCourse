5b86e8e5250928aacecdaff8f6e0f73e476ece13
Laila Ibrahim <laila457@h-eng.helwan.edu.eg>
2022-06-17 19:04:16 +0200

    commit

--- solve/app/app.php
+++ solve/app/app.php
@@ -3,3 +3,54 @@
 declare(strict_types = 1);
 
 // Your Code
+function getTransactionFiles(string $dirPath):array{
+    $files=[];
+    foreach(scandir($dirPath) as $file){
+        // var_dump($file);
+        if(is_dir($file)){
+            continue;
+        }
+        $files[]=$dirPath . $file;
+    }
+    return $files;
+}
+function getTransactions(string $fileName):array{
+    if(! file_exists($fileName)){
+        trigger_error('File "' . $fileName . '"does not exist',E_USER_ERROR);
+
+    }
+    $file=fopen($fileName,'r');
+    fgetcsv($file);
+    $transations =[];
+    while(($transation =fgetcsv($file)) !== false){
+        // $transations[]=$transation;
+       
+        $transations[]=extractTransaction($transation);
+    }
+    return $transations;
+}
+function extractTransaction(array $transactionRow):array{
+    [$date,$checkNumber,$description,$amount]=$transactionRow;
+    $amount=(float) str_replace(['$',','],'',$amount);
+    return[
+      'date' =>$date,
+      'checkNumber'=>$checkNumber,
+      'description'=>$description,
+      'amount'=>$amount,
+    ];
+}
+function calculateTotals(array $transations):array{
+    $totals =['netTotal'=>0,'totalIncome'=>0,'totalExpense'=>0];
+   
+    foreach($transations as $transation){
+        $totals['netTotal'] += $transation['amount'];
+
+        if($transation['amount']>= 0){
+            $totals['totalIncome'] += $transation['amount'];
+        }else{
+            $totals['totalExpense'] += $transation['amount'];
+        }
+
+    }
+    return $totals;
+}
--- solve/public/index.php
+++ solve/public/index.php
@@ -9,3 +9,16 @@ define('FILES_PATH', $root . 'transaction_files' . DIRECTORY_SEPARATOR);
 define('VIEWS_PATH', $root . 'views' . DIRECTORY_SEPARATOR);
 
 /* YOUR CODE (Instructions in README.md) */
+
+require APP_PATH . "app.php";
+require APP_PATH .'helpers.php';
+$files= getTransactionFiles(FILES_PATH);
+$transactions =[];
+foreach($files as $file){
+$transactions = array_merge($transactions,getTransactions($file));
+}
+// print_r($files); // Array ( [0] => sample_1.csv )
+//  var_dump($files);//array(1) { [0]=> string(82) "C:\xampp\htdocs\simpleApp\simpleApp_phpCourse\solve\transaction_files\sample_1.csv" }
+// print_r($transactions);
+$totals=calculateTotals($transactions);
+require VIEWS_PATH . 'transactions.php'; 
